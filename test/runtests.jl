73d03da3a6516d0d89acee4fa3de51450184ee8a
Joby M. Anthony III <62029937+jmanthony3@users.noreply.github.com>
2024-01-10 10:40:36 -0500

    setup test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,35 +1,51 @@
 using NumericalMethods
+using StaticArrays
 using Test
 
 @testset "NumericalMethods.jl" begin
-    # # ENGR 705-001: Finite Element Analysis - Homework 3
-    # ## 3.1
-    # A, E, L = 4, 30e6, 30 # [in², psi, in]
-    # elements = Dict("1"=>(1, 2), "2"=>(2, 3), "3"=>(3, 4))
-    # nodeboundaryconditions = (1, 4)
-    # F_applied = [(2, 5e3), (3, -10e3)]
-    # preparedelements = prepareelements_line(elements, A, E, L)
-    # U = solve(preparedelements, nodeboundaryconditions, F_applied).U
-    # @test round.(U, digits=6) == [0, 0, -0.00125, 0]
-    # ## 3.2
-    # A, E, L = 4e-4, 210e9, 2 # [m², Pa, m]
-    # elements = Dict("1"=>(1, 2), "2"=>(2, 3))
-    # nodeboundaryconditions = [1, (3, 25e-3, 2)]
-    # F_applied = [(2, -5e3)]
-    # preparedelements = prepareelements_line(elements, A, E, L)
-    # U = solve(preparedelements, nodeboundaryconditions, F_applied).U
-    # @test round.(U, digits=6) == [0, 0.012440, 0.025]
-    # ## 3.3
-    # A, E, L = 1, 10e6, 100          # [in², psi, in]
-    # angles = [120, 0, 210]          # [°]
-    # L = L ./ abs.(cosd.(angles))
-    # elements = Dict("1"=>(1, 2), "2"=>(1, 3), "3"=>(1, 4))
-    # nodeboundaryconditions = [2, 3, 4]
-    # F_applied = [(1, (1e3, 1e3))]   # (node, (force_x [lb], force_y [lb]))
-    # preparedelements = prepareelements_line(elements, A, E, L, angles, dims=2)
-    # sol = solve(preparedelements, nodeboundaryconditions, F_applied, dims=2)
-    # σ = axialstresses(preparedelements, sol.U, dims=2)
-    # @test round.(σ, digits=6) == [-577.350269, -422.649731, 1000]
-
     # Write your tests here.
+    ## derivatives
+    h, a, b = 0.25, 1, 2
+    x       = [a:h:b...]
+    y       = (x .^ 2) .* cos.(x)
+    n       = length(x)
+    xstatic = SVector{n, AbstractFloat}(x)
+    ystatic = SVector{n, AbstractFloat}(y)
+    @test round(n1derivative(x, y, 2), digits=6)                    == -0.690759 # ... 4120392901
+    @test round(n1derivative(xstatic, ystatic, 2), digits=6)        == -0.690759 # ... 4120392901
+    @test round(endpoint(x, y, h, :begin), digits=6)                ==  0.381398 # ... 2872273567
+    @test round(endpoint(xstatic, ystatic, h, :begin), digits=6)    ==  0.381398 # ... 2872273567
+    @test round(midpoint(x, y, h, 2), digits=6)                     == -0.762287 # ... 2042316164
+    @test round(midpoint(xstatic, ystatic, h, 2), digits=6)         == -0.762287 # ... 2042316164
+
+    ## integration
+    @test round(integrate(y, x, rule=:trapezoidal), digits=6)       == -0.114043 # ... 79264796141
+    @test round(integrate(y, x, rule=:simpson13),   digits=6)       == -0.084893 # ... 08746263464
+    @test round(integrate(y, x, rule=:simpson38),   digits=6)       == -0.213298 # ... 30449138726
+    @test round(integrate(y, x, rule=:simpsonN),    digits=6)       == -0.084893 # ... 08746263458
+    @test round(integrate(y, x, rule=:midpoint),    digits=6)       == -0.017729 # ... 11806132075
+
+    ## interpolation
+    x       = [0.01, 0.15, 0.31, 0.5, 0.6, 0.75]
+    y       = [1.0, 1.004, 1.031, 1.117, 1.223, 1.422]
+    degree  = 2
+    @test round(linearleastsquares(x, y, degree)[2], digits=6)      == 0.000873 # ... 6264091126483
+
+    x       = float.([0,  5,  9, 12, 16, 23, 28])   # days
+    y1      = float.([5, 14, 39, 34, 28, 26, 25])   # mg
+    y2      = float.([5, 13, 15, 14, 12, 11, 10])   # mg
+    @test lagrange(x, y1)[2][end]                                   == 0.
+    @test lagrange(x, y2)[2][end]                                   == 0.
+    n       = length(x)
+    xstatic = SVector{n, AbstractFloat}(x)
+    y1stat  = SVector{n, AbstractFloat}(y1)
+    y2stat  = SVector{n, AbstractFloat}(y2)
+    @test lagrange(xstatic, y1stat)[2][end]                         == 0.
+    @test lagrange(xstatic, y2stat)[2][end]                         == 0.
+
+    x   = [0.0:0.1:0.5...]
+    y   = sin.(3 .* x)
+    dx  = 3cos.(3 .* x)
+    @test round(clamped(x, y, dx)[2][begin](0.05),  digits=6)       == 0.149437 # ... 07092796263
+    @test round(natural(x, y)[2][begin](0.05),      digits=6)       == 0.149408 # ... 86040416965
 end
