8e23b8cfc298121281d58ab9511c5f6ca237db78
Joby M. Anthony III <62029937+jmanthony3@users.noreply.github.com>
2024-01-11 14:37:44 -0500

    fix typos and homogenize formatting

--- src/functions.jl
+++ src/functions.jl
@@ -38,8 +38,6 @@ function lagrange(
             g_eval          = build_function(g, t, expression=Val{false})
             gp              = simplify(expand_derivatives(Dt(g)); expand=true)
             gp_eval         = build_function(gp, t, expression=Val{false})
-            # println(("\tg", i, n, g))
-            # println(("\tgp", i, n, gp))
             coeffs_sorted   = if hasproperty(Symbolics.value(gp), :dict)
                 coeffs_map      = Symbolics.value(gp).dict
                 exps            = Symbolics.degree.(keys(coeffs_map))
@@ -50,8 +48,6 @@ function lagrange(
             if gp_eval(0.) != 0.
                 pushfirst!(coeffs_sorted, gp_eval(0.))
             end
-            # println(("\tcoeffs", i, n, coeffs_sorted))
-            # println(("\tpolynomial", i, n, Polynomial(coeffs_sorted)))
             g_prime_poly    = Polynomial(float.(coeffs_sorted))
             gx              = if i == 1
                 g_eval(coeffs_sorted[begin])
@@ -66,17 +62,13 @@ function lagrange(
                 end
                 maximum(abs.(R))
             end
-            # println(("\tξ", i, n, simplify(prod(ξ); expand=true)))
             for k ∈ 1:1:i
                 ξ = simplify(expand_derivatives(Dt(ξ)); expand=true)
-                # println(("\tξ", i, k, n, ξ))
             end
-            # println(("\tξ / n!", i, n, ξ / (factorial(n))))
-            Dξ = maximum(build_function(ξ, t; expression=Val{false}).(x[begin:i]) ./ (factorial(n)))
-            # println((i, n, Dξ, abs(gx)))
-            ξ_error[i] = Dξ * abs(gx)
-            # xi_err = maximum(xi_error)
-            # # println((i, n, xi_err))
+            Dξ              = maximum(
+                build_function(ξ, t; expression=Val{false}).(x[begin:i])
+                    ./ (factorial(n)))
+            ξ_error[i]      = Dξ * abs(gx)
         end
         return maximum(abs.(ξ_error))
     end
@@ -84,13 +76,9 @@ function lagrange(
     terms   = []
     errors  = zeros(MVector{degree + 1})
     for k ∈ 1:1:degree + 1
-        # println(("degree", k, degree))
-        # println(("term", k, term(domain[k], range[k], t)))
         push!(terms, f[k] * coefficient(x[k], t))
-        # println(("terms", k, sum(terms)))
         errors[k]   = error(k, sum(terms), t)
     end
-    # return nothing, nothing
     polynomial = build_function(sum(terms), t, expression=Val{false})
     return polynomial, errors
     # return polynomial
@@ -98,7 +86,7 @@ end
 
 ## 3.3 (p. 122)
 """
-    newtondifference(x, f, α[; var::Symbol=:x, dir::Symbol=:auto])
+    newtondifference(x, f, α[; dir::Symbol=:auto])
 
 Given a domain and range, construct some polynomial by Newton's Divided Difference.
 `'forward'` or `'backward'` construction. Will be chosen automatically if not specified.
@@ -111,7 +99,6 @@ function newtondifference(
     x   ::T,
     f   ::T,
     α   ::Real;
-    var ::Symbol        = :x,
     dir ::Symbol        = :auto
 ) where {T<:AbstractVector}
     if dir == :auto
@@ -124,16 +111,11 @@ function newtondifference(
     fxn[:, 1], fxn[:, 2] = x, f
     for j ∈ 2:1:m
         for i ∈ j:1:m
-            # println((i, j))
             fₖ = fterm(fxn, i, j)
             fxn[i, j + 1] = fₖ
             if dir == :forward && i == j
-                # println((i, j, fₖ))
-                # push!(coeff, fₖ)
                 coeff[j - 1] = fₖ
             elseif dir == :backward && i == m
-                # println((i, j, fₖ))
-                # push!(coeff, fₖ)
                 coeff[j - 1] = fₖ
             end
         end
@@ -151,13 +133,13 @@ end
 
 ## 3.5 (p. 142)
 """
-    natural(domain, f, function_derivative)
+    natural(x, f)
 
 The bookend polynomials do not assume the slope entering and exiting the interval as the derivative at the respective endpoint.
 """
 function natural(
-    domain              ::T,
-    f                   ::T
+    x   ::T,
+    f   ::T
 )::Tuple{AbstractVector, AbstractArray} where {T<:AbstractVector}
     function _algorithm(g)
         Y               = g
@@ -197,7 +179,7 @@ function natural(
         end
         return Y, A, B, C, D
     end
-    g, X            = f, domain
+    g, X            = f, x
     Y, A, B, C, D   = _algorithm(g)
     n, splines      = length(X) - 1, []
     for j ∈ 1:1:n
@@ -209,14 +191,14 @@ function natural(
 end
 
 """
-    clamped(domain, f, function_derivative)
+    clamped(x, f, fp)
 
 The bookend polynomials will have the same slope entering and exiting the interval as the derivative at the respective endpoint.
 """
 function clamped(
-    domain              ::T,
-    f                   ::T,
-    function_derivative ::T
+    x   ::T,
+    f   ::T,
+    fp  ::T
 )::Tuple{AbstractVector, AbstractArray} where {T<:AbstractVector}
     function _algorithm(g, gp)
         Y, YP           = g, gp
@@ -260,7 +242,7 @@ function clamped(
         end
         return Y, A, B, C, D
     end
-    g, X, gp        = f, domain, function_derivative
+    g, X, gp        = f, x, fp
     Y, A, B, C, D   = _algorithm(g, gp)
     n, splines      = length(X) - 1, []
     for j ∈ 1:1:n
@@ -292,7 +274,6 @@ end
 
 # Ch. 4 (p. 171)
 ## 4.1 (p. 172)
-
 function n1derivative(
     x       ::T,
     f       ::T,
@@ -311,8 +292,8 @@ function n1derivative(
         end
         return prod(num) / prod(den)
     end
-    degree = (isnothing(degree) ? length(x) - 1 : degree)
-    gp = 0.
+    degree  = (isnothing(degree) ? length(x) - 1 : degree)
+    gp      = 0.
     for k ∈ 1:1:degree + 1
         Lₖ          = coefficient(x[k], x)
         Lₖp         = simplify(expand_derivatives(Dt(Lₖ)); expand=true)
@@ -323,36 +304,36 @@ function n1derivative(
 end
 
 """
-    endpoint(x, y, h, point[, point_type="three"])
+    endpoint(x, f, h, point[, point_type="three"])
 
 Find the derivative of a bookend point at either `:begin` or `:end` of dataset.
-Acceptable values for `point_type` include {"three", "five"}.
+Acceptable values for `method` include {`:three`, `:five`}.
 """
 function endpoint(
-    x           ::T,
-    y           ::T,
-    h           ::Real,
-    point       ::Symbol;
-    point_type  ::String    = "three"
+    x       ::T,
+    f       ::T,
+    h       ::Real,
+    point   ::Symbol;
+    method  ::Symbol    = :three
 )::AbstractFloat where {T<:AbstractVector}
     i = (point == :begin ? 1 : (point == :end ? length(x) : nothing))
     if point == :begin
-        if point_type == "three"
-            (-3y[i] + 4y[i+1] - y[i+2]) / 2h
-        elseif point_type == "five"
-            (-25y[i] + 48y[i+1]
-                - 36y[i+2] + 16y[i+3]
-                    - 3y[i+4]) / 12h
+        if method == :three
+            (-3f[i] + 4f[i+1] - f[i+2]) / 2h
+        elseif method == :five
+            (-25f[i] + 48f[i+1]
+                - 36f[i+2] + 16f[i+3]
+                    - 3f[i+4]) / 12h
         else
             NaN
         end
     elseif point == :end
-        if point_type == "three"
-            -(-3y[i] + 4y[i-1] - y[i-2]) / 2h
-        elseif point_type == "five"
-            -(-25y[i] + 48y[i-1]
-                - 36y[i-2] + 16y[i-3]
-                    - 3y[i-4]) / 12h
+        if method == :three
+            -(-3f[i] + 4f[i-1] - f[i-2]) / 2h
+        elseif method == :five
+            -(-25f[i] + 48f[i-1]
+                - 36f[i-2] + 16f[i-3]
+                    - 3f[i-4]) / 12h
         else
             NaN
         end
@@ -362,26 +343,26 @@ function endpoint(
 end
 
 """
-    midpoint(x, y, h, point[, point_type="three"])
+    midpoint(x, f, h, point[, point_type="three"])
 
 Find the derivative of some point within a dataset.
-Acceptable values for `point_type` include {"three", "five", "2nd_derivative"}.
+Acceptable values for `method` include {`:three`, `:five`, `:2nd`}.
 """
 function midpoint(
-    x           ::T,
-    y           ::T,
-    h           ::Real,
-    point       ::Integer;
-    point_type  ::String    = "three"
+    x       ::T,
+    f       ::T,
+    h       ::Real,
+    point   ::Integer;
+    method  ::Symbol    = :three
 )::AbstractFloat where {T<:AbstractVector}
     i = point
-    return if point_type == "three"
-        (y[i + 1] - y[i - 1]) / 2h
-    elseif point_type == "five"
-        (y[i - 2] - 8y[i - 1]
-            + 8y[i + 1] - y[i + 2]) / 12h
-    elseif point_type == "2nd_derivative"
-        (y[i - 1] - 2y[i] + y[i + 1]) / (h ^ 2.)
+    return if method == :three
+        (f[i + 1] - f[i - 1]) / 2h
+    elseif method == :five
+        (f[i - 2] - 8f[i - 1]
+            + 8f[i + 1] - f[i + 2]) / 12h
+    elseif method == :2nd
+        (f[i - 1] - 2f[i] + f[i + 1]) / (h ^ 2.)
     else
         NaN
     end
@@ -542,6 +523,7 @@ struct ODE
     b   ::Real
     h   ::Real
     α   ::Real
+    β   ::Real
     N   ::Integer
     # vars::AbstractVector{Num}
 end
@@ -555,50 +537,45 @@ Solve `obj` according to `method` ∈ {`:forward_euler` (default), `:backward_eu
 Each method has an equivalent convenience function.
 E.g. `ivp(obj; method=:runge_kutta)` ≡ `runge_kutta(obj)`.
 """
-function ivp(obj::ODE, tol=10^-3; method=:forward_euler)
+function ivp(obj::ODE; tol=10^-3, method=:forward_euler)
     f       = obj.f
-    a, b, h = obj.a, obj.b, obj.h
-    # vars    = obj.vars
-    t, w0   = a, obj.α
+    t, h, w = obj.a, obj.h, obj.α
     ea, eb, λ = 1/2, 1/2, 1
     g       = zeros(MVector{obj.N + 1})
-    g[1]    = w0
-    # g       = [w0]
+    g[1]    = w
     for i ∈ 1:1:obj.N
-        w = w0 + if method == :foward_euler
-            h * f(t, w0)
+        w += if method == :forward_euler
+            h * f(t, w)
         elseif method == :backward_euler
-            h * f(t + h, w0 + h*f(t, w0))
+            h * f(t + h, w + h*f(t, w))
         elseif method ∈ [:improved_euler, :modified_euler]
-            h * (ea*f(t, w0) + eb*f(t + λ*h, w0 + λ*h*f(t, w0)))
+            h * (ea*f(t, w) + eb*f(t + λ*h, w + λ*h*f(t, w)))
         elseif method == :runge_kutta
-            k1 = h * f(t,       w0)
-            k2 = h * f(t + h/2, w0 + k1/2)
-            k3 = h * f(t + h/2, w0 + k2/2)
-            k4 = h * f(t + h,   w0 + k3)
+            k1 = h * f(t,       w)
+            k2 = h * f(t + h/2, w + k1/2)
+            k3 = h * f(t + h/2, w + k2/2)
+            k4 = h * f(t + h,   w + k3)
             (k1 + 2k2 + 2k3 + k4) / 6
         end
-        # push!(g, w)
         g[i + 1] = w
         # push!(increments, w - w0)
-        w0  = w
-        t   = a + i*h
+        t   = obj.a + i*h
     end
     return g
 end
 
-forward_euler(obj, tol=10^-3) = ivp(obj, tol; method=:forward_euler)
-backward_euler(obj, tol=10^-3) = ivp(obj, tol; method=:backward_euler)
-improved_euler(obj, tol=10^-3) = ivp(obj, tol; method=:improved_euler)
-modified_euler(obj, tol=10^-3) = ivp(obj, tol; method=:modified_euler)
-runge_kutta(obj, tol=10^-3) = ivp(obj, tol; method=:runge_kutta)
+forward_euler(obj;  tol=10^-3) = ivp(obj; tol=tol, method=:forward_euler)
+backward_euler(obj; tol=10^-3) = ivp(obj; tol=tol, method=:backward_euler)
+improved_euler(obj; tol=10^-3) = ivp(obj; tol=tol, method=:improved_euler)
+modified_euler(obj; tol=10^-3) = ivp(obj; tol=tol, method=:modified_euler)
+runge_kutta(obj;    tol=10^-3) = ivp(obj; tol=tol, method=:runge_kutta)
 
 # Ch. 8 (p. 505)
 ## 8.1 (p. 506)
 """
-    linearleastsquares(domain, f, degree::Integer)
+    linearleastsquares(x, f, n::Integer)
 
-Construct a polynomial of some degree while minimizing the least squares error.
+Construct a polynomial of some degree, n while minimizing the least squares error.
 
 # Notes
 Least squares error := ``E = \\sum_{i=1}^{m}[y_{i} - P_{n}(x_{i})]^{2}``
@@ -606,15 +583,16 @@ Least squares error := ``E = \\sum_{i=1}^{m}[y_{i} - P_{n}(x_{i})]^{2}``
 Constructed polynomial of the form: ``P(x) = a_{n}x^{n} + a_{n - 1}x^{n - 1} + \\dots + a_{1}x + a_{0}``
 """
 function linearleastsquares(
-    domain  ::T,
-    f       ::T,
-    degree  ::Integer
+    x   ::T,
+    f   ::T,
+    n   ::Integer
 ) where {T<:AbstractVector}
-    X, Y            = domain, f
-    m               = length(X)
-    A, b            = zeros((degree+1, degree+1)), zeros(degree+1)
-    for i ∈ 0:1:degree
-        for j ∈ 0:1:degree
+    X, Y    = x, f
+    m       = length(X)
+    A       = MMatrix{n+1, n+1}(zeros((n+1, n+1)))
+    b       = zeros(MVector{n+1})
+    for i ∈ 0:1:n
+        for j ∈ 0:1:n
             for k ∈ 1:1:m
                 A[i+1,j+1] += X[k]^(i + j)
             end
@@ -624,13 +602,13 @@ function linearleastsquares(
         end
     end
     @variables t
-    x, polynomial   = A\b, 0
-    for i ∈ 0:1:degree
-        polynomial += x[i+1]*t^i
+    x, p    = A\b, 0.
+    for i ∈ 0:1:n
+        p  += x[i+1]*t^i
     end
-    polynomial      = build_function(polynomial, t, expression=Val{false})
-    error           = sum((Y - polynomial.(X)).^2)
-    return polynomial, error
+    p       = build_function(p, t, expression=Val{false})
+    error   = sum((Y - p.(X)).^2)
+    return p, error
 end
 
 """
@@ -639,26 +617,29 @@ end
 Given a domain and range, yield the coefficients for an equation and the equation of the form ``y = ax^{b}``.
 """
 function linearleastsquares(
-    domain  ::T,
+    x       ::T,
     f       ::T,
     type    ::Symbol
 ) where {T<:AbstractVector}
     if type == :power
-        X, Y            = domain, f
-        m               = length(X)
-        q1, q2, q3, q4  = [], [], [], []
+        X, Y    = x, f
+        m       = length(X)
+        q1      = zeros(MVector{m})
+        q2      = zeros(MVector{m})
+        q3      = zeros(MVector{m})
+        q4      = zeros(MVector{m})
         for i ∈ 1:1:m
-            push!(q1, log(X[i])*log(Y[i]))
-            push!(q2, log(X[i]))
-            push!(q3, log(Y[i]))
-            push!(q4, log(X[i])^2)
+            q1[i] = log(X[i])*log(Y[i])
+            q2[i] = log(X[i])
+            q3[i] = log(Y[i])
+            q4[i] = log(X[i])^2
         end
-        num             = m*sum(q1) - sum(q2)*sum(q3)
-        den             = m*sum(q4) - (sum(q2))^2
-        b               = num / den
-        a               = exp((sum(q3) - b*sum(q2)) / m)
-        expression(x)   = a*(x^b)
-        error           = sum((Y - expression.(X)) .^ 2)
-        return expression, error
+        num     = m*sum(q1) - sum(q2)*sum(q3)
+        den     = m*sum(q4) - sum(q2)^2
+        b       = num / den
+        a       = exp((sum(q3) - b*sum(q2)) / m)
+        p(x)    = a*(x^b)
+        error   = sum((Y - p.(X)) .^ 2)
+        return p, sum((Y - p.(X)) .^ 2)
     end
 end
